9fb7be14cbb5fecb13bc67745b45ee6045276600
tankooni <tankooni@gmail.com>
2014-11-21 23:26:41 -0500

    Yay stuff

---
+++ MissTaryGame/MissTaryGame/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Croanc
+ * Date: 11/21/2014
+ * Time: 10:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Indigo;
+
+namespace MissTaryGame
+{
+	class Game : Engine
+	{
+		public static float Lol = 10;
+
+		public static void Main(string[] args)
+		{
+			var game = new Game();
+			game.Run();
+	    }
+
+		public Game() :
+			base(640, 480, 60)
+		{
+			FP.Console.Enable();
+
+			FP.World = new StartScreenWorld();
+			FP.Screen.ClearColor = FP.Color(0x6495ED);
+			
+			FP.Console.Enable();
+
+		}
+
+		public override void FocusLost()
+		{
+			base.FocusLost();
+			this.Paused = true;
+		}
+
+		public override void FocusGained()
+		{
+			base.FocusGained();
+			if(!FP.Console.IsOpen)
+				this.Paused = false;
+		}
+	}
+}
---
+++ MissTaryGame/MissTaryGame/Worlds/StartScreenWorld.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Croanc
+ * Date: 11/21/2014
+ * Time: 11:19 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Indigo;
+using Indigo.Inputs;
+using Indigo.Graphics;
+
+namespace MissTaryGame
+{
+	/// <summary>
+	/// Description of StartScreenGame.
+	/// </summary>
+	public class StartScreenWorld : World
+	{
+		private Text start;
+        private Text instructions;
+		public StartScreenWorld()
+		{
+            start = new Text("Start [Enter]");
+            start.X = (FP.Width / 2) - (start.Width / 2);
+            start.Y = (FP.Height / 3) + 25;
+
+            instructions = new Text("Instructions [Space]");
+            instructions.X = (FP.Width / 2) - (instructions.Width / 2);
+            instructions.Y = (FP.Height / 3) + 50;
+
+            AddGraphic(start);
+            AddGraphic(instructions);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+//            if (Keyboard.Return.Pressed)
+//                FP.World = new GameWorld();
+//
+//            if (Keyboard.Space.Pressed)
+//                FP.World = new InstructionsScreenWorld();
+		}
+	}
+}
