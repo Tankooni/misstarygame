d048c3fb2ded80b6cd9479c6d5527da71ca39e45
tankooni <tankooni@gmail.com>
2014-11-22 14:32:06 -0500

    Stuff

---
+++ MissTaryGame/MissTaryGame/Entities/InteractiveObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Indigo;
+using Indigo.Graphics;
+
+namespace MissTaryGame
+{
+	public class InteractiveObject : Entity
+	{
+		/// <summary>
+		/// 2D array representitive of how to scale the object, each value should be between 0 to 1
+		/// Should be the same size as the scene
+		/// </summary>
+		public int[,] PerspectiveMap { get; set; }
+		public Image sprite { get; set; }
+		public InteractiveObject()
+		{
+			sprite = new Indigo.Graphics.Spritemap(Library.GetTexture("content/Avatar/Idle/Idle1.png"), 148, 332);
+			
+//			sprite.OriginX = 74;
+//			sprite.OriginY = 166;
+			this.OriginX = 74;
+			this.OriginY = 166;
+			AddComponent(sprite);
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+		}
+		
+		public override void Render()
+		{
+			sprite.Scale = PerspectiveMap[this.X, this.Y];
+			base.Render();
+		}
+	}
+}
---
+++ MissTaryGame/MissTaryGame/Worlds/DynamicSceneWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Indigo;
+using Indigo.Graphics;
+namespace MissTaryGame
+{
+	public class DynamicSceneWorld : World
+	{
+		public DynamicSceneWorld()
+		{
+			var avatar = new InteractiveObject();
+			this.Add(avatar);
+			Texture texture = Library.GetTexture("content/ExampleScene/ExampleScene_Perspective.png");
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+			
+		}
+	}
+}
