9bf464b3ed2a7e4c9f5e4b204b5824e83aa77783
tankooni <tankooni@gmail.com>
2014-11-23 11:52:32 -0500

    Linden forgot to add something when he was committing last commit from my machine.

---
+++ MissTaryGame/MissTaryGame/Json/Models/Actions/ActionTriggerEvent.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Indigo;
+using MissTaryGame;
+using System.Linq;
+
+namespace MissTaryGame.Json.Models.Actions
+{
+	/// <summary>
+	/// Description of ActionTriggerEvent.
+	/// </summary>
+	public class ActionTriggerEvent : IAction
+	{
+		public string eventName;
+		
+		public ActionTriggerEvent(Dictionary<string, Object> args)
+		{
+			eventName = (string)args["Event"];
+		}
+		
+		public void run() {
+			var world = (DynamicSceneWorld)FP.World;
+			GameEvent evt;
+			
+			if(world.uncompletedEvents.ContainsKey(eventName)) {
+				evt = world.uncompletedEvents[eventName];
+				
+				
+				world.completedEvents[eventName] = evt;
+				world.uncompletedEvents.Remove(eventName);
+				
+				foreach( var a in world.completedEvents[eventName].Actions ) {
+					a.run();
+				}
+			}
+		}
+	}
+}
