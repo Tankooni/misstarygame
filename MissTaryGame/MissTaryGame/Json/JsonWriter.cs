3047a78d96092bc86dcd1cae429f2c9c78663998
Tankooni <Tankooni@gmail.com>
2015-11-14 14:56:51 -0500

    q

---
+++ MissTaryGame/MissTaryGame/Json/JsonWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MissTaryGame.Json
+{
+    public static class JsonWriter
+    {
+        public static void Save()
+        {
+            var ss = JsonConvert.SerializeObject(Utility.MainConfig);
+            File.WriteAllText(JsonLoader.PATH_PREFIX + "save/MainConfig" + JsonLoader.RESOURCE_EXT, ss);
+        }
+        //public static WriteObjectToDisk<T>(string path)
+        //{
+
+        //}
+    }
+}
